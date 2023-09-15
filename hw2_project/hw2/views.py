3382310c0e72d9e973dd488dc7ef1cccb3d8f545
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2023-09-15 11:24:55 +0300

    finished HW4

--- hw2_project/hw2/views.py
+++ hw2_project/hw2/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from .forms import ImageForm
 
-# Create your views here.
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+    else:
+        form = ImageForm()
+    return render(request, 'hw2/upload_image.html', {'form': form})
