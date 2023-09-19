dc39cf828592b16c6f027356ed2d800bede1ed5b
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2023-09-19 19:20:42 +0300

    Delete hw2_project directory

--- hw2_project/hw2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import product_by_order, customer_orders
-
-
-urlpatterns = [
-    path('customer/<int:customer_id>/', customer_orders, name='customer_orders'),
-    path('product/<int:product_id>/', product_by_order, name='product_by_orders'),
-    ]
